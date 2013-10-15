92106d37d92a72bc492892d8a580872b90862bad
compwron <wordymusic@gmail.com>
2013-10-14 17:34:23 -0700

    both run

---
+++ src/test/spike/SeedDataTest.java
@@ -0,0 +1,11 @@
+package spike;
+
+import org.junit.Test;
+
+public class SeedDataTest {
+
+    @Test
+    public void shouldLoadSeedData() throws Exception {
+        System.out.println("Seed data seeded");
+    }
+}
