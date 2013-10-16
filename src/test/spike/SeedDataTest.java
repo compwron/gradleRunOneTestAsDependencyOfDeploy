764c20284fd1be81aeac77c8de82a601e27c62c0
compwron <wordymusic@gmail.com>
2013-10-16 08:40:56 -0700

    Appears to be working

--- src/test/spike/SeedDataTest.java
+++ src/test/spike/SeedDataTest.java
@@ -2,10 +2,15 @@ package spike;
 
 import org.junit.Test;
 
+import static org.junit.Assert.fail;
+
 public class SeedDataTest {
 
     @Test
     public void shouldLoadSeedData() throws Exception {
         System.out.println("Seed data seeded");
+        fail("This test ran");
     }
+
+
 }
